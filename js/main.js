738e05410c43e44e9ba80015a515bd135aaa34de
Vivek Gahunia <vgahunia@gmail.com>
2016-06-10 17:05:26 -0400

    search by state and county instead of zip

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 
 window.onload = function() {
 	var x = document.getElementById("target");
-	var lat,lng, zip;
 	function getLocation() {
 	    if (navigator.geolocation) {
 	        navigator.geolocation.getCurrentPosition(showPosition);
@@ -10,8 +9,8 @@ window.onload = function() {
 	    }
 	}
 	function showPosition(position) {
-			lat = position.coords.latitude;
-	    lng = position.coords.longitude;
+			var lat = position.coords.latitude;
+	    var lng = position.coords.longitude;
 	    getAddress(lat,lng);
 	}
 	getLocation();
@@ -24,16 +23,22 @@ window.onload = function() {
       // turn coordinates into an object
       var yourLocation = new google.maps.LatLng(latitude, longitude);
    
-      // find out info about our location
+      // GET USER'S COUNTY and STATE info from API
       geocoder.geocode({ 'latLng': yourLocation }, function (results, status) {
       if(status == google.maps.GeocoderStatus.OK) {
         if(results[0]) {
           $('#target').fadeIn(function() {
-            let postalCode = results[0].address_components.find(function (component) {
-                return component.types[0] == "postal_code";
-            });
-            zip = postalCode.long_name;
-            getData(zip);
+            let countyName = results[0].address_components.find(function (component) {
+                return component.types[0] == "administrative_area_level_2";
+              });
+            let stateName = results[0].address_components.find(function (component) {
+                return component.types[0] == "administrative_area_level_1";
+              });
+            countyFull = countyName.long_name.toLowerCase();
+            var c = countyFull.split(" ");
+            var county = c[0];
+            var state = stateName.long_name.toLowerCase();
+            getData(county, state);
           })
         } else {
           error('Google did not return any results.');
@@ -44,40 +49,46 @@ window.onload = function() {
     });
   }
 
+// MATCHING DATA from data.js and SENDING DATA to VIEW
   var data1 = JSON.stringify(housingData);
   var data = JSON.parse(data1);
-  function getData(zipCode) {
+
+  function getData(county, state) {
     for (var i=0; i<data.length;i++) {
-      if (data[i].zip == zipCode) {
+      if (data[i].county.toLowerCase() == county && data[i].state.toLowerCase() == state) {
         var data1 = data[i].airPollution;
         var data2 = data[i].waterViolation;
         var data3 = data[i].housingProblems;
         var data4 = data[i].driveAlone;
         var data5 = data[i].driveAloneLongCommute;
-        writeData(data1, data2, data3, data4, data5);
+        var data6 = data[i].state
+        writeData(county, state, data1, data2, data3, data4, data5);
       }
     }
-    console.log(zipCode);
+    console.log(county, data6, data1, data2, data3, data4, data5);
   }
-  function writeData(air, water, housing, drive, driveLong) {
-    $('#target').html(zip);
-    $('#zipInput').show();
+  function writeData(writeCounty, writeState, air, water, housing, drive, driveLong) {
+    $('#target').html(writeCounty + ' County, ' + writeState);
+    $('#countyInput').show();
     $('#dataPoint1 p').html(air);
     $('#dataPoint2 p').html(water);
     $('#dataPoint3 p').html(housing);
     $('#dataPoint4 p').html(drive);
     $('#dataPoint5 p').html(driveLong);
   }
+// END
 
-  function newZip() {
-    var newZipValue = document.getElementById('zipValue').value;
-    getData(newZipValue);
+//RECEIVING DATA from HOME PAGE FORM
+  function newCounty() {
+    var newCountyValue = document.getElementById('countyValue').value.toLowerCase();
+    var newStateValue = document.getElementById('stateValue').value.toLowerCase();
+    getData(newCountyValue, newStateValue);
   }
 
-  $('#zipClick').click(function() {
-    newZip();
+  $('#countyClick').click(function() {
+    newCounty();
   })
-  
+// END
 }
 
 
