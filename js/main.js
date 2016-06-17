7f9be3e17ec1cf4257d62aab86948b0f42fb1485
Vivek Gahunia <vgahunia@gmail.com>
2016-06-17 14:43:17 -0400

    added error handling

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ window.onload = function() {
 	var x = document.getElementById("target");
 	function getLocation() {
 	    if (navigator.geolocation) {
-	        navigator.geolocation.getCurrentPosition(showPosition);
+	        navigator.geolocation.getCurrentPosition(showPosition, showError);
 	    } else {
 	        x.innerHTML = "Geolocation is not supported by this browser.";
 	    }
@@ -13,6 +13,22 @@ window.onload = function() {
 	    var lng = position.coords.longitude;
 	    getAddress(lat,lng);
 	}
+  function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            x.innerHTML = "User denied the request for Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            x.innerHTML = "Location information is unavailable."
+            break;
+        case error.TIMEOUT:
+            x.innerHTML = "The request to get user location timed out."
+            break;
+        case error.UNKNOWN_ERROR:
+            x.innerHTML = "An unknown error occurred."
+            break;
+    }
+}
 	getLocation();
 
   // use Google Maps API to reverse geocode users' location
