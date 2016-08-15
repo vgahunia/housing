de45431bb98ba017a0d3cbf257deca706c0e8847
Vivek Gahunia <vgahunia@gmail.com>
2016-08-14 23:55:08 -0400

    added zip searchability and prev-next buttons

--- js/main.js
+++ js/main.js
@@ -1,121 +1,152 @@
 
 window.onload = function() {
 
-	var x = document.getElementById("target");
-	function getLocation() {
-	    if (navigator.geolocation) {
-	        navigator.geolocation.getCurrentPosition(showPosition, showError);
-	    } else {
-	        x.innerHTML = "Geolocation is not supported by this browser.";
-	    }
-	}
-	function showPosition(position) {
-			var lat = position.coords.latitude;
-	    var lng = position.coords.longitude;
-	    getAddress(lat,lng);
-	}
-  function showError(error) {
-    switch(error.code) {
-        case error.PERMISSION_DENIED:
-            x.innerHTML = "User denied the request for Geolocation. Please enter information in the fields below."
-            break;
-        case error.POSITION_UNAVAILABLE:
-            x.innerHTML = "Location information is unavailable. Please enter information in the fields below."
-            break;
-        case error.TIMEOUT:
-            x.innerHTML = "The request to get user location timed out. Please enter information in the fields below."
-            break;
-        case error.UNKNOWN_ERROR:
-            x.innerHTML = "An unknown error occurred. Please enter information in the fields below."
-            break;
-    }
-}
-	getLocation();
-
-  // use Google Maps API to reverse geocode users' location
-  function getAddress(latitude, longitude) {
-      // set up the Geocoder object
-      var geocoder = new google.maps.Geocoder();
-  
-      // turn coordinates into an object
-      var yourLocation = new google.maps.LatLng(latitude, longitude);
-   
-      // GET USER'S COUNTY and STATE info from API
-      geocoder.geocode({ 'latLng': yourLocation }, function (results, status) {
-      if(status == google.maps.GeocoderStatus.OK) {
-        if(results[0]) {
-          $('#target').fadeIn(function() {
-            let countyName = results[0].address_components.find(function (component) {
-                return component.types[0] == "administrative_area_level_2";
-              });
-            let stateName = results[0].address_components.find(function (component) {
-                return component.types[0] == "administrative_area_level_1";
-              });
-            countyFull = countyName.long_name.toLowerCase();
-            var c = countyFull.split(" ");
-            var county = c[0];
-            var state = stateName.long_name.toLowerCase();
-            getData(county, state);
-          })
-        } else {
-          error('Google did not return any results.');
-        }
-      } else {
-        error("Reverse Geocoding failed due to: " + status);
-      }
-    });
-  }
-
 // MATCHING DATA from data.js and SENDING DATA to VIEW
   var data1 = JSON.stringify(housingData);
   var data = JSON.parse(data1);
 
   var data2 = JSON.stringify(zipData);
   var dataZip = JSON.parse(data2);
-  console.log(dataZip[2].zip)
 
   String.prototype.capitalize = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
-}
-  function getData(county, state) {
-    for (var i=0; i<data.length;i++) {
-      if (data[i].county.toLowerCase() == county && data[i].state.toLowerCase() == state) {
-        var data1 = data[i].airPollution;
-        var data2 = data[i].waterViolation;
-        var data3 = data[i].housingProblems;
-        var data4 = data[i].driveAlone;
-        var data5 = data[i].driveAloneLongCommute;
-        var data6 = data[i].state;
-        county =county.capitalize()
-        state=state.capitalize();
-        writeData(county, state, data1, data2, data3, data4, data5);
+  }
+
+  function getZip(zip) {
+    console.log(zip)
+    for (var i=0; i<dataZip.length;i++) {
+      if (dataZip[i].zip === zip) {
+        getData(dataZip[i].county)
       }
     }
-    console.log(county, data6, data1, data2, data3, data4, data5);
   }
-  function writeData(writeCounty, writeState, air, water, housing, drive, driveLong) {
-    $('#target').html(writeCounty + ' County, ' + writeState);
-    $('#countyInput').show();
+  function getData(county) {
+    var data2;
+    var newCounty = Number(county);
+    for (var i=0; i<data.length;i++) {
+      if (data[i].fips == newCounty) {
+        var data1 = data[i].airPollution;
+        if (data[i].waterViolation === "") {
+          data2 = "N/A";
+        } else {
+          data2 = data[i].waterViolation;
+        };
+        var data3 = data[i].housingProblems;
+        var data4 = data[i].driveAlone;
+        writeData(data1, data2, data3, data4);
+      }
+    }
+  }
+  function writeData(air, water, housing, drive) {
     $('#dataPoint1 p').html(air);
     $('#dataPoint2 p').html(water);
     $('#dataPoint3 p').html(housing);
     $('#dataPoint4 p').html(drive);
-    $('#dataPoint5 p').html(driveLong);
   }
 // END
 
 //RECEIVING DATA from HOME PAGE FORM
-  function newCounty() {
-    var newCountyValue = document.getElementById('countyValue').value.toLowerCase();
-    var newStateValue = document.getElementById('stateValue').value.toLowerCase();
-    getData(newCountyValue, newStateValue);
+  function newZip() {
+    var newZipValue = document.getElementById('zipValue').value;
+    getZip(newZipValue);
   }
 
-  $('#countyClick').click(function() {
-    newCounty();
+  $('#zipClick').click(function() {
+    newZip();
   })
-// END
+
+  $('#next').on('click', getNext);
+  $('#prev').on('click', getPrev);
+
+  function getNext() {
+    var $curr = $('.show div:visible'),
+        $next = ($curr.next().length) ? $curr.next() : $('.show div').first();
+    transition($curr, $next);
+  }
+
+  function getPrev() {
+    var $curr = $('.show div:visible'),
+        $next = ($curr.prev().length) ? $curr.prev() : $('.show div').last();
+    transition($curr, $next);
+  }
+
+  function transition($curr, $next) {
+    $next.css('z-index', 2).fadeIn('slow', function () {
+        $curr.hide().css('z-index', 0);
+        $next.css('z-index', 1);
+    });
+  }
 }
+// END
+//  var x = document.getElementById("target");
+//  function getLocation() {
+//      if (navigator.geolocation) {
+//          navigator.geolocation.getCurrentPosition(showPosition, showError);
+//      } else {
+//          x.innerHTML = "Geolocation is not supported by this browser.";
+//      }
+//  }
+//  function showPosition(position) {
+//      var lat = position.coords.latitude;
+//      var lng = position.coords.longitude;
+//      getAddress(lat,lng);
+//  }
+//   function showError(error) {
+//     switch(error.code) {
+//         case error.PERMISSION_DENIED:
+//             x.innerHTML = "User denied the request for Geolocation. Please enter information in the fields below."
+//             break;
+//         case error.POSITION_UNAVAILABLE:
+//             x.innerHTML = "Location information is unavailable. Please enter information in the fields below."
+//             break;
+//         case error.TIMEOUT:
+//             x.innerHTML = "The request to get user location timed out. Please enter information in the fields below."
+//             break;
+//         case error.UNKNOWN_ERROR:
+//             x.innerHTML = "An unknown error occurred. Please enter information in the fields below."
+//             break;
+//     }
+// }
+//  getLocation();
+
+//   // use Google Maps API to reverse geocode users' location
+//   function getAddress(latitude, longitude) {
+//       // set up the Geocoder object
+//       var geocoder = new google.maps.Geocoder();
+  
+//       // turn coordinates into an object
+//       var yourLocation = new google.maps.LatLng(latitude, longitude);
+   
+//       // GET USER'S COUNTY and STATE info from API
+//       geocoder.geocode({ 'latLng': yourLocation }, function (results, status) {
+//       if(status == google.maps.GeocoderStatus.OK) {
+//         if(results[0]) {
+//           $('#target').fadeIn(function() {
+//             let countyName = results[0].address_components.find(function (component) {
+//                 return component.types[0] == "administrative_area_level_2";
+//               });
+//             let stateName = results[0].address_components.find(function (component) {
+//                 return component.types[0] == "administrative_area_level_1";
+//               });
+//             let zipName = results[0].address_components.find(function (component) {
+//                 return component.types[0] == "postal_code";
+//               });
+//             countyFull = countyName.long_name.toLowerCase();
+//             var c = countyFull.split(" ");
+//             var county = c[0];
+//             var state = stateName.long_name.toLowerCase();
+//             var geoZip = zipName.long_name;
+//             console.log(geoZip);
+//             getZip(geoZip);
+//           })
+//         } else {
+//           error('Google did not return any results.');
+//         }
+//       } else {
+//         error("Reverse Geocoding failed due to: " + status);
+//       }
+//     });
+//   }
 
 
 //Google Maps Key - AIzaSyCFQqre9L6qnIiGrpvkQMdMK-c6Z5D5HJU
